f88a9d9408a6089d54b490cba778b8b416132a7c
ebocher <ebocher@irstv>
2009-06-25 07:56:07 +0000

    Morphological types

---
+++ src/org/jdelaunay/delaunay/TopoType.java
@@ -0,0 +1,35 @@
+package org.jdelaunay.delaunay;
+
+public class TopoType {
+
+	//Topographic types
+
+	public static final String UPTALWEG = "upTalweg";
+	public static final String DOWNTALWEG = "downTalweg";
+	public static final String FLATTALWEG = "flatTalweg";
+	public static final String UPRIDGE = "upridge";
+	public static final String DOWNRIDGE = "downridge";
+	public static final String FLATRIDGE = "flatridge";
+	public static final String FLAT = "flat";
+	public static final String UPRIGHTSLOPE = "upRightSlope";
+	public static final String DOWNRIGHTSLOPE = "downRightSlope";
+	public static final String FLATRIGHTSLOPE = "flatRightSlope";
+	public static final String UPLEFTTSLOPE = "upLeftSlope";
+	public static final String DOWNLEFTSLOPE = "downLeftSlope";
+	public static final String FLATLEFTSLOPE = "flatLeftSlope";
+	public static final String BORDER = "border";
+	public static final String RIGHTSIDE = "rightside";
+	public static final String LEFTSIDE = "leftside";
+	public static final String RIGHTWELL = "rightwell";
+	public static final String LEFTWELL = "leftwell";
+	public static final String UPLEFTTCOLINEAR = "upLeftColinear";
+	public static final String DOWNLEFTCOLINEAR = "downLeftColinear";
+	public static final String FLATLEFTCOLINEAR = "flatLeftColinear";
+	public static final String UPRIGHTCOLINEAR = "upRightColinear";
+	public static final String DOWNRIGHTCOLINEAR = "downRightColinear";
+	public static final String FLATRIGHTCOLINEAR = "flatRightColinear";
+	public static final String UPDOUBLECOLINEAR = "upDoubleColinear";
+	public static final String DOWNDOUBLECOLINEAR = "downDoubleColinear";
+	public static final String FLATDOUBLECOLINEAR = "flatDoubleColinear";
+
+}
