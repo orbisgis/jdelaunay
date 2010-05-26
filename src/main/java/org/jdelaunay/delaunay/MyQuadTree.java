bd3ed8e499872c12f9e5dd42099388a6e2e24ddb
jeanyves <jeanyves@irstv>
2010-05-26 11:00:59 +0000

    Minor changes

--- src/main/java/org/jdelaunay/delaunay/MyQuadTree.java
+++ src/main/java/org/jdelaunay/delaunay/MyQuadTree.java
@@ -97,20 +97,19 @@ public class MyQuadTree<T extends MyElement> {
 		MyBox saveBox = new MyBox();
 
 		// Check in which area of the QuadTree it can be
-		// Elements at the frontier may be counted several times -> the will be inserted in the root
+		// Elements at the frontier may be counted several times -> the will be
+		// inserted in the root
 		for (int sector = 0; sector < 4; sector++) {
 			MyBox testBox = getSector(sector, boundingBox);
-			if ((((theBox.minx >= testBox.minx) && (theBox.minx <= testBox.maxx))
-					|| ((theBox.maxx >= testBox.minx) && (theBox.maxx <= testBox.maxx)))
-					&& (((theBox.miny >= testBox.miny) && (theBox.miny <= testBox.maxy))
-							|| ((theBox.maxy >= testBox.miny) && (theBox.maxy <= testBox.maxy)))) {
+			if ((((theBox.minx >= testBox.minx) && (theBox.minx <= testBox.maxx)) || ((theBox.maxx >= testBox.minx) && (theBox.maxx <= testBox.maxx)))
+					&& (((theBox.miny >= testBox.miny) && (theBox.miny <= testBox.maxy)) || ((theBox.maxy >= testBox.miny) && (theBox.maxy <= testBox.maxy)))) {
 				// One point at least is inside => memorize it
 				indexIntersect = sector;
 				NbPointsInside++;
 				saveBox = new MyBox(testBox);
 			}
 		}
-		
+
 		// Insert it or go further
 		if ((NbPointsInside == 0) || (NbPointsInside > 1))
 			theList.add(element);
@@ -144,15 +143,18 @@ public class MyQuadTree<T extends MyElement> {
 			double x = aPoint.getX();
 			double y = aPoint.getY();
 
-			// test bounding box of the each subarea and search inside if it is in the
+			// test bounding box of the each subarea and search inside if it is
+			// in the
 			// box
 			int i = 0;
 			while ((i < 4) && (anElement == null)) {
-				MyBox testBox = getSector(i, boundingBox);
-				if ((x >= testBox.minx) && (x <= testBox.maxx)
-						&& (y >= testBox.miny) && (y <= testBox.maxy)) {
-					// One point at least is inside => memorize it
-					anElement = search(aPoint, testBox);
+				if (theQuadTree[i] != null) {
+					MyBox testBox = getSector(i, boundingBox);
+					if ((x >= testBox.minx) && (x <= testBox.maxx)
+							&& (y >= testBox.miny) && (y <= testBox.maxy)) {
+						// One point at least is inside => memorize it
+						anElement = theQuadTree[i].search(aPoint, testBox);
+					}
 				}
 				i++;
 			}
