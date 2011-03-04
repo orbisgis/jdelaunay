4f44182f0cb09c1f56ff39adfb55fd78563037b5
agueganno <agueganno@irstv>
2011-03-04 15:35:57 +0000

    adding tests that watch over VoronoiGraph processing.

--- src/test/java/org/jdelaunay/delaunay/TestVoronoiGraph.java
+++ src/test/java/org/jdelaunay/delaunay/TestVoronoiGraph.java
@@ -65,4 +65,145 @@ public class TestVoronoiGraph extends BaseUtility{
 			}
 		}
 	}
+
+	/**
+	 * The same test as above, but we change our starting node.
+	 * @throws DelaunayError
+	 */
+	public void testProcessGraphOtherStartNode() throws DelaunayError {
+		List<DTriangle> tris = TestVoronoiNodes.getSampleTriangles();
+		DTriangle tri = tris.get(2);
+		VoronoiGraph vg = new VoronoiGraph(tri);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==10);
+		for(VoronoiNode vn : vg.getSortedNodes()){
+			boolean found = false;
+			for(DTriangle dt : tris){
+				if(vn.getParent().equals(dt)){
+					found = true;
+					break;
+				}
+			}
+			if(!found){
+				assertTrue(false);
+			}
+		}
+	}
+
+	/**
+	 * We isolate a triangle and check we obtain a graph with a single node.
+	 * @throws DelaunayError
+	 */
+	public void testProcessGraphIsolatedTriangle() throws DelaunayError {
+		List<DTriangle> tris = TestVoronoiNodes.getSampleTriangles();
+		DTriangle tri = tris.get(9);
+		tri.getEdges()[0].setLocked(true);
+		tri.getEdges()[1].setLocked(true);
+		tri.getEdges()[2].setLocked(true);
+		VoronoiGraph vg = new VoronoiGraph(tri);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==1);
+		assertTrue(vg.getSortedNodes().contains(new VoronoiNode(new DTriangle(new DEdge(2,6,0,4,3,0),
+					new DEdge(4,3,0,5,6,0), new DEdge(5,6,0,2,6,0)))));
+	}
+
+	/**
+	 * We isolate a triangle and check we obtain a graph with a single node.
+	 * @throws DelaunayError
+	 */
+	public void testProcessNotTotallyIsolatedTriangle() throws DelaunayError {
+		List<DTriangle> tris = TestVoronoiNodes.getSampleTriangles();
+		DTriangle tri = tris.get(9);
+		tri.getEdges()[0].setLocked(true);
+		tri.getEdges()[1].setLocked(true);
+		VoronoiGraph vg = new VoronoiGraph(tri);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==10);
+		for(VoronoiNode vn : vg.getSortedNodes()){
+			boolean found = false;
+			for(DTriangle dt : tris){
+				if(vn.getParent().equals(dt)){
+					found = true;
+					break;
+				}
+			}
+			if(!found){
+				assertTrue(false);
+			}
+		}
+	}
+
+	/**
+	 * We isolate two triangle and check we obtain a graph with 8 nodes.
+	 * @throws DelaunayError
+	 */
+	public void testProcessTwoIsolatedTriangles() throws DelaunayError {
+		List<DTriangle> tris = TestVoronoiNodes.getSampleTriangles();
+		DTriangle tri1 = tris.get(2);
+		tri1.getEdges()[0].setLocked(true);
+		tri1.getEdges()[1].setLocked(true);
+		tri1.getEdges()[2].setLocked(true);
+		DTriangle tri2 = tris.get(3);
+		tri2.getEdges()[0].setLocked(true);
+		tri2.getEdges()[1].setLocked(true);
+		tri2.getEdges()[2].setLocked(true);
+		DTriangle tri = tris.get(9);
+		VoronoiGraph vg = new VoronoiGraph(tri);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==8);
+		for(VoronoiNode vn : vg.getSortedNodes()){
+			boolean found = false;
+			for(DTriangle dt : tris){
+				if(vn.getParent().equals(dt) || vn.getParent().equals(tri1)||vn.getParent().equals(tri2)){
+					found = true;
+					break;
+				}
+			}
+			if(!found){
+				assertTrue(false);
+			}
+		}
+	}
+
+	/**
+	 * test that we retrieve two isolated graphs when dividing the space in
+	 * two parts with constrained edges.
+	 * @throws DelaunayError
+	 */
+	public void testTwoInnerGraphs() throws DelaunayError {
+		List<DTriangle> tris = TestVoronoiNodes.getSampleTriangles();
+		DTriangle tri1 = tris.get(2);
+		DTriangle tri2 = tris.get(3);
+		for(int i=0; i<DTriangle.PT_NB;i++){
+			if(tri1.getEdges()[i].equals(new DEdge(5,6,0,1,9,0))){
+				tri1.getEdges()[i].setLocked(true);
+			}
+			if(tri2.getEdges()[i].equals(new DEdge(5,6,0,8,3,0))){
+				tri2.getEdges()[i].setLocked(true);
+			}
+		}
+		DTriangle tri = tris.get(9);
+		VoronoiGraph vg = new VoronoiGraph(tri);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==8);
+		for(VoronoiNode vn : vg.getSortedNodes()){
+			boolean found = false;
+			for(DTriangle dt : tris){
+				if(vn.getParent().equals(dt) || vn.getParent().equals(tri1)||vn.getParent().equals(tri2)){
+					found = true;
+					break;
+				}
+			}
+			if(!found){
+				assertTrue(false);
+			}
+		}
+		vg = new VoronoiGraph(tri1);
+		vg.fillUntilNotFlatFound();
+		assertTrue(vg.getSortedNodes().size()==2);
+		assertTrue(vg.getSortedNodes().contains(new VoronoiNode(new DTriangle(new DEdge(1,9,0,5,6,0),
+					new DEdge(5,6,0,8,6,0), new DEdge(8,6,0,1,9,0)))));
+		assertTrue(vg.getSortedNodes().contains(new VoronoiNode(new DTriangle(new DEdge(8,3,0,5,6,0),
+					new DEdge(5,6,0,8,6,0), new DEdge(8,6,0,8,3,0)))));
+	}
 }
