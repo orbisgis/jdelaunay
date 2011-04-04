cf63ff765c57eee32fb83b49c0b935808c47faa3
agueganno <agueganno@irstv>
2011-04-04 11:34:29 +0000

    adding a test on morphological qualification.

--- src/test/java/org/jhydrocell/hydronetwork/TestHydroTinBuilder.java
+++ src/test/java/org/jhydrocell/hydronetwork/TestHydroTinBuilder.java
@@ -37,4 +37,43 @@ public class TestHydroTinBuilder extends BaseUtility{
 		assertTrue(prop.contentEquals("river,talweg")||prop.contentEquals("talweg,river"));
 		assertTrue(mesh.getEdges().get(2).getProperty()==(HydroProperties.RIVER+HydroProperties.TALWEG));
 	}
+
+	public void testMorphologicalQualifDeeper() throws DelaunayError {
+		DEdge e1 = new DEdge(0,0,3,4,2,3);
+		DEdge e2 = new DEdge(2,1,5,6,3,8);
+		DEdge e3 = new DEdge(2,2,4,6,0,3);
+		DEdge e4 = new DEdge(6,2,3,9,2,7);
+		e1.setProperty(HydroProperties.WALL);
+		e2.setProperty(HydroProperties.WALL);
+		e3.setProperty(HydroProperties.WALL);
+		e4.setProperty(HydroProperties.WALL);
+		HydroTINBuilder mesh = new HydroTINBuilder();
+		mesh.addConstraintEdge(e1);
+		mesh.addConstraintEdge(e2);
+		mesh.addConstraintEdge(e3);
+		mesh.addConstraintEdge(e4);
+		mesh.forceConstraintIntegrity();
+		for(DEdge ed : mesh.getConstraintEdges()){
+			assertTrue(ed.hasProperty(HydroProperties.WALL));
+		}
+		assertTrue(mesh.getConstraintEdges().size()==7);
+		mesh.processDelaunay();
+		int i =0;
+		for(DEdge ed : mesh.getEdges()){
+			if(mesh.getConstraintEdges().contains(ed)){
+				assertTrue(ed.hasProperty(HydroProperties.WALL));
+				i++;
+			}
+		}
+		assertTrue(i==7);
+		mesh.morphologicalQualification();
+		i =0;
+		for(DEdge ed : mesh.getEdges()){
+			if(mesh.getConstraintEdges().contains(ed)){
+				assertTrue(ed.hasProperty(HydroProperties.WALL));
+				i++;
+			}
+		}
+		assertTrue(i==7);
+	}
 }
