a5c327148e8b340a7f3935682c28b887300bc1e3
agueganno <agueganno@irstv>
2011-05-13 09:52:22 +0000

    remove useless import

--- src/main/java/org/jdelaunay/delaunay/ConstrainedMesh.java
+++ src/main/java/org/jdelaunay/delaunay/ConstrainedMesh.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
