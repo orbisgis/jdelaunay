0518e646790d3a97e1c72d4f33e1d2a2ca7b992f
Antoine Gourlay <antoine@gourlay.fr>
2012-06-29 09:23:14 +0200

    update license headers to new legal format.

--- src/main/java/org/jdelaunay/delaunay/error/DelaunayError.java
+++ src/main/java/org/jdelaunay/delaunay/error/DelaunayError.java
@@ -1,30 +1,30 @@
-/*
- * jDelaunay is a library dedicated to the processing of Delaunay and constrained 
+/**
+ *
+ * jDelaunay is a library dedicated to the processing of Delaunay and constrained
  * Delaunay triangulations from PSLG inputs.
- * 
- * This library is developed at French IRSTV institute as part of the AvuPur and Eval-PDU project, 
- * funded by the French Agence Nationale de la Recherche (ANR) under contract 
+ *
+ * This library is developed at French IRSTV institute as part of the AvuPur and Eval-PDU project,
+ * funded by the French Agence Nationale de la Recherche (ANR) under contract
  * ANR-07-VULN-01 and ANR-08-VILL-0005-01 .
- * 
+ *
  * jDelaunay is distributed under GPL 3 license. It is produced by the "Atelier SIG" team of
- * the IRSTV Institute <http://www.irstv.cnrs.fr/> CNRS FR 2488.
- * 
- * Copyright (C) 2010 Erwan BOCHER, Alexis GUEGANNO, Adelin PIAU, Jean-Yves MARTIN
- * Copyright (C) 2011 Alexis GUEGANNO, Jean-Yves MARTIN
- * 
+ * the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2010-2012 IRSTV FR CNRS 2488
+ *
  * jDelaunay is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * jDelaunay is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * jDelaunay. If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, please consult: <http://trac.orbisgis.org/>
+ *
+ * For more information, please consult: <http://www.orbisgis.org/>
  * or contact directly:
  * info_at_ orbisgis.org
  */
@@ -34,7 +34,9 @@ package org.jdelaunay.delaunay.error;
  * The exception that is used to describe the kind of errors that can happen when
  * building or using a ConstrainedMesh or its elements in this library.
  * 
- * @author Jean-Yves MARTIN, Erwan BOCHER, Alexis GUÉGANNO
+ * @author Jean-Yves Martin
+ * @author Erwan Bocher
+ * @author Alexis Guéganno
  */
 public class DelaunayError extends Exception {
 
